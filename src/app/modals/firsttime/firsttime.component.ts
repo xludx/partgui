fb1fecca623229054af143481ecf3e49f72dce7e
Ryno Mathee <rmathee@gmail.com>
2017-06-07 22:58:12 +0200

    Progress commit

---
+++ src/app/modals/firsttime/firsttime.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'modal-firsttime',
+  templateUrl: './firsttime.component.html',
+  styleUrls: ['./firsttime.component.scss']
+})
+export class FirsttimeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/modals/syncing/syncing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-syncing',
+  templateUrl: './syncing.component.html',
+  styleUrls: ['./syncing.component.scss']
+})
+export class SyncingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
